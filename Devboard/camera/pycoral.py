2f3d56e40840aa154dc2ca57c6cc8e73d46b7fd4
kaburia <kaburiaaustin1@gmail.com>
2023-01-10 13:06:11 +0300

    interpreter prediction

--- Devboard/camera/pycoral.py
+++ Devboard/camera/pycoral.py
@@ -6,8 +6,8 @@ from mediapipe_functs import mediapipe_detection, draw_styled_landmarks, extract
 import os
 
 # Loading saved test points
-X_test = np.load('x_test.npy')
-y_test = np.load('y_test.npy')
+# X_test = np.load('x_test.npy')
+# y_test = np.load('y_test.npy')
 
 # SAVED_MODEL_PATH 
 TFLITE_FILE_PATH = 'action5.tflite'
@@ -16,15 +16,16 @@ TFLITE_FILE_PATH = 'action5.tflite'
 interpreter = tf.lite.Interpreter(model_path=TFLITE_FILE_PATH)
 
 # Get input and output tensors.
+interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Resize to initial form for predictions
 # interpreter.resize_tensor_input(input_details[0]['index'], (18, 30,126))
 # interpreter.resize_tensor_input(output_details[0]['index'], (18, 3))
-interpreter.allocate_tensors()
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+
+# input_details = interpreter.get_input_details()
+# output_details = interpreter.get_output_details()
 
 '''
 Input Shape: [ 18  30 126]
@@ -33,8 +34,8 @@ Output Shape: [18  3]
 Output Type: <class 'numpy.float32'>
 '''
 
-test_imgs_numpy = np.array(X_test, dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], test_imgs_numpy)
+# test_imgs_numpy = np.array(X_test, dtype=np.float32)
+# interpreter.set_tensor(input_details[0]['index'], test_imgs_numpy)
 interpreter.invoke()
 tflite_model_predictions = interpreter.get_tensor(output_details[0]['index'])
 print(tflite_model_predictions)
@@ -69,7 +70,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
         # Make detections
         image, results = mediapipe_detection(frame, holistic)
-        print(results)
+        # print(results)
         
         # Draw landmarks
         draw_styled_landmarks(image, results)
@@ -82,5 +83,39 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         sequence = sequence[-30:]
 
         if len(sequence) == 30:
-              res
+            test = np.expand_dims(np.array(sequence, dtype=np.float32), axis=0)
+            interpreter.set_tensor(input_details[0]['index'], test)
+            interpreter.invoke()
+          
+            res = interpreter.get_tensor(output_details[0]['index'])
+            print(res)
+            print(np.argmax(res))
+            print(actions[np.argmax(res)])
+
+        #3. Viz logic
+            if res[np.argmax(res)] > threshold: 
+                if len(sentence) > 0: 
+                    if actions[np.argmax(res)] != sentence[-1]:
+                        sentence.append(actions[np.argmax(res)])
+                else:
+                    sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+
+            # Viz probabilities
+            image = prob_viz(res, actions, image, colors)
+            
+        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, ' '.join(sentence), (3,30), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
