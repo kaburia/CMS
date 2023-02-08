24109119c11fe4780cb97229dd02c50f13a11b97
kaburia <kaburiaaustin1@gmail.com>
2023-02-08 15:40:26 +0300

    edgeTPU

---
+++ install.sh
@@ -0,0 +1,15 @@
+# /bin/bash
+
+sudo apt -y install libportaudio2
+pip install -q --use-deprecated=legacy-resolver tflite-model-maker
+pip install -q pycocotools
+pip install -q opencv-python-headless==4.1.2.30
+pip uninstall -y tensorflow && pip install -q tensorflow==2.8.0
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+
+sudo apt-get update
+
+sudo apt-get install edgetpu-compiler
