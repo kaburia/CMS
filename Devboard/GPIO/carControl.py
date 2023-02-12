e74bc2342599336f925997c6e5811d5f77704be0
kaburia <kaburiaaustin1@gmail.com>
2023-02-12 13:00:54 +0000

    Servo pins

--- Devboard/GPIO/carControl.py
+++ Devboard/GPIO/carControl.py
@@ -6,15 +6,13 @@ GPIO.setmode(GPIO.BOARD) #determine  pin-numbering schemes BOARD= pin numbers fo
 GPIO.setwarnings(False)
 
 #...................................ULTRASONIC SENSOR SETUP....................................................
-GPIO_TRIGGER = 29 #USE GPIO_5 AS trigger
-GPIO_ECHO = 31 #USE GPIO_6 AS Echo 
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT) # set trigger pin as output
-GPIO.setup(GPIO_ECHO,GPIO.IN) #set echo pin as input
+
 
 #......................................SERVO SETUP............................................................
-PWM_PIN=32 #use GPIO12(BCM) pin 32(*BOARD)
+PWM_PIN=33 #use GPIO12(BCM) pin 32(*BOARD)
 GPIO.setup(PWM_PIN,GPIO.OUT)
-Servo = GPIO.PWM(PWM_PIN,50) #50hz pwm frequency to get a period of 20ms that a servo needs
+Servo = GPIO.PWM(PWM_PIN,50)  #50hz pwm frequency to get a period of 20ms that a servo needs
+Servo.start(0)
 
 #....................................MOTOR DRIVER............................................................
 class Drive(): #to call the objects functions  moveF ,moveB,STOP,LEFT,RIGHT
@@ -31,51 +29,61 @@ class Drive(): #to call the objects functions  moveF ,moveB,STOP,LEFT,RIGHT
         GPIO.setup(self.Enb,GPIO.OUT)
         GPIO.setup(self.In3,GPIO.OUT)
         GPIO.setup(self.In4,GPIO.OUT)
-        self.pwm = GPIO.PWM(self.Ena,1000) #set pwm pin and a frequency of 1kHz
-        self.pwm = GPIO.PWM(self.Enb,1000)
-        self.pwm.start(0)
+        self.pwma = GPIO.PWM(self.Ena,1000) #set pwm pin and a frequency of 1kHz
+        self.pwmb = GPIO.PWM(self.Enb,1000)
+        self.pwma.start(0)
+        self.pwmb.start(0)
 
 
-    def moveF(self,speed=80,Time =10): #move Forward motors rotate in the same direction  to call it........... Drive1.moveF(Duty Cycle ,Time to move forward).........
-        GPIO.output(self.In1,GPIO.LOW)
-        GPIO.output(self.In2,GPIO.HIGH)
-        GPIO.output(self.In3,GPIO.LOW)
-        GPIO.output(self.In4,GPIO.HIGH)
-        pwm.ChangeDutyCycle(speed)
-        sleep(Time)
-    def moveB(self,speed=80,Time =2): #move ackward motors rotate in the oposite direction to call it........... Drive1.move(Duty Cycle ,Time to reverse).........
+    def moveF(self,speed=80,Time =5): #move Forward motors rotate in the same direction  to call it........... Drive1.moveF(Duty Cycle ,Time to move forward).........
         GPIO.output(self.In1,GPIO.HIGH)
         GPIO.output(self.In2,GPIO.LOW)
         GPIO.output(self.In3,GPIO.HIGH)
         GPIO.output(self.In4,GPIO.LOW)
-        pwm.ChangeDutyCycle(speed)
+        self.pwma.ChangeDutyCycle(speed)
+        self.pwmb.ChangeDutyCycle(speed)
+        sleep(Time)
+    def moveB(self,speed=80,Time =5): #move ackward motors rotate in the oposite direction to call it........... Drive1.move(Duty Cycle ,Time to reverse).........
+        GPIO.output(self.In1,GPIO.LOW)
+        GPIO.output(self.In2,GPIO.HIGH)
+        GPIO.output(self.In3,GPIO.LOW)
+        GPIO.output(self.In4,GPIO.HIGH)
+        self.pwma.ChangeDutyCycle(speed)
+        self.pwmb.ChangeDutyCycle(speed)
         sleep(Time)
     def STOP(self,Time =2):  #stop motors puling in pins low stops the lm298 to call it........... Drive1.STOP(STOP time else it will stop for 2 sec).........
         GPIO.output(self.In1,GPIO.LOW) #selt in1 high
         GPIO.output(self.In2,GPIO.LOW)
         GPIO.output(self.In3,GPIO.LOW)
         GPIO.output(self.In4,GPIO.LOW)
-        pwm.ChangeDutyCycle(0) #0% pwm duty cycle
+        self.pwm.ChangeDutyCycle(0) #0% pwm duty cycle
         sleep(Time) 
     def LEFT(self,Time=0.5): #move left to call it........... Drive1.LEFT(Time for this left movement ).........
         GPIO.output(self.In1,GPIO.HIGH)
         GPIO.output(self.In2,GPIO.LOW)
-        GPIO.output(self.In3,GPIO.LOW)
-        GPIO.output(self.In4,GPIO.HIGH)
-        pwm.ChangeDutyCycle(80)
+        GPIO.output(self.In3,GPIO.HIGH)
+        GPIO.output(self.In4,GPIO.LOW)
+        self.pwmb.ChangeDutyCycle(50)
+        self.pwma.ChangeDutyCycle(100)
         
         sleep(Time)
     def RIGHT(self,Time =0.5): #move left to call it........... Drive1.RIGHT(Time for this left movement ).........
-        GPIO.output(self.In1,GPIO.LOW)
-        GPIO.output(self.In2,GPIO.HIGH)
+        GPIO.output(self.In1,GPIO.HIGH)
+        GPIO.output(self.In2,GPIO.LOW)
         GPIO.output(self.In3,GPIO.HIGH)
         GPIO.output(self.In4,GPIO.LOW)
-        pwm.ChangeDutyCycle(90)
+        self.pwma.ChangeDutyCycle(50)
+        self.pwmb.ChangeDutyCycle(100)
+        # self.pwma.ChangeDutyCycle(speed)
         sleep(Time)  
 
-Drive1 = Drive(13,14,16,33,18,20)
+
 #....................................GET DISTANCE ULTRASONIC..........................................................
 def Distance():
+    GPIO_TRIGGER = 29 #USE GPIO_5 AS trigger
+    GPIO_ECHO = 31 #USE GPIO_6 AS Echo 
+    GPIO.setup(GPIO_TRIGGER,GPIO.OUT) # set trigger pin as output
+    GPIO.setup(GPIO_ECHO,GPIO.IN) #set echo pin as input
    # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
    # set Trigger after 0.01ms to LOW
@@ -107,6 +115,10 @@ def Estimate_Time(D):
 
 #...........................................SERVO MOTOR CONTROL..............................................................
 def SERVO(Angle):
-  servo.ChangeDutyCycle(Angle)
-  time.sleep(1)
+    
+    Servo.ChangeDutyCycle(Angle)
+    time.sleep(3)
   
+
+
+
