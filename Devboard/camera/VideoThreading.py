cc198d385daf2c3d3e03cc1cc5d94cf9920fba43
kaburia <kaburiaaustin1@gmail.com>
2023-02-12 12:35:53 +0000

    increasing FPS

---
+++ Devboard/camera/VideoThreading.py
@@ -0,0 +1,78 @@
+import cv2 
+import time
+from threading import Thread
+from datetime import datetime
+
+
+class CountsPerSec:
+    """
+    Class that tracks the number of occurrences ("counts") of an
+    arbitrary event and returns the frequency in occurrences
+    (counts) per second. The caller must increment the count.
+    """
+
+    def __init__(self):
+        self._start_time = None
+        self._num_occurrences = 0
+
+    def start(self):
+        self._start_time = datetime.now()
+        return self
+
+    def increment(self):
+        self._num_occurrences += 1
+
+    def countsPerSec(self):
+        elapsed_time = (datetime.now() - self._start_time).total_seconds()
+        return self._num_occurrences / elapsed_time
+
+class VideoGet:
+    def __init__(self, src=cv2.CAP_V4L2):
+        self.stream = cv2.VideoCapture(src)
+        self.ret, self.frame = self.stream.read()
+        self.stopped = False
+    
+    def start(self):
+        Thread(target=self.get, args=()).start()
+        return self
+    
+    def get(self):
+        while not self.stopped:
+            if not self.ret:
+                self.stop()
+            else:
+                self.ret, self.frame = self.stream.read()
+    
+    def stop(self):
+        self.stopped = True
+
+def putIterationsPerSec(frame, iterations_per_sec):
+    """
+    Add iterations per second text to lower-left corner of a frame.
+    """
+
+    cv2.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
+        (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
+    return frame
+
+class VideoShow:
+    """
+    Class that continuously shows a frame using a dedicated thread.
+    """
+
+    def __init__(self, frame=None):
+        self.frame = frame
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.show, args=()).start()
+        return self
+
+    def show(self):
+        while not self.stopped:
+            cv2.imshow("Video", self.frame)
+            if cv2.waitKey(1) == ord("q"):
+                self.stopped = True
+
+    def stop(self):
+        self.stopped = True
