5a730e0fb32972657cf709d57669115c7993274a
Austin Kaburia <88529649+kaburia@users.noreply.github.com>
2023-02-18 11:51:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
-# Install and setup Python3.8
-sudo apt update
-sudo apt install -y python3.8
+# Update and upgrade the system
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Install Python 3.8 and make it the default version
+sudo apt-get install -y python3.8
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 sudo update-alternatives --set python /usr/bin/python3.8
 
-# Install necessary packages
-sudo apt install -y python3-pip python3-opencv libopencv-dev libjasper-dev libqtgui4 libqt4-test libhdf5-dev libhdf5-103 libatlas-base-dev libjasper-dev libqtgui4 libqt4-test libilmbase-dev libopenexr-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libtbb-dev libdc1394-22-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libopenblas-dev
+# Install raspi-config
+sudo apt-get install -y raspi-config
 
-# Install pip packages
-sudo pip3 install numpy pandas matplotlib scikit-learn scipy imutils pillow tensorflow keras pycoral RPi.GPIO adafruit-blinka
-
-# Enable SPI and I2C interfaces
+# Enable camera and GPIO pins using raspi-config
+sudo raspi-config nonint do_camera 0
 sudo raspi-config nonint do_spi 0
 sudo raspi-config nonint do_i2c 0
 
-# Clone YOLOv5 repository
-git clone https://github.com/ultralytics/yolov5.git
-cd yolov5
+# Install dependencies for ultrasonic sensor
+sudo apt-get install -y python3-dev python3-rpi.gpio
 
-# Install YOLOv5 dependencies
-sudo pip3 install -r requirements.txt
+# Install dependencies for servo motor control
+sudo apt-get install -y pigpio
 
-# Download YOLOv5 weights
+# Install dependencies for DC motor control
+sudo apt-get install -y python3-pigpio
+
+# Install PyCoral and EdgeTPU tools for Coral Dev Board
+wget https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp38-cp38-linux_aarch64.whl
+sudo apt-get install -y python3-pip
+sudo python3 -m pip install numpy pillow opencv-python
+sudo python3 -m pip install tflite_runtime-2.5.0-cp38-cp38-linux_aarch64.whl
+
+# Download and convert YOLOv5s model for Coral Dev Board
 wget https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5s.pt
+python3 -m tflite_convert --output_file=yolov5s_edgetpu.tflite --saved_model_dir=yolov5s.pt --enable_v1_converter
 
-# Clone Adafruit_Python_PCA9685 repository for controlling the servo motor
-git clone https://github.com/adafruit/Adafruit_Python_PCA9685.git
-cd Adafruit_Python_PCA9685
-sudo python3 setup.py install
-cd ..
+# Enable pigpiod daemon at startup
+sudo systemctl enable pigpiod.service
 
-# Start the program with multithreading support
-python3 main.py -t
-
+echo "Setup complete. Please reboot your Raspberry Pi."
