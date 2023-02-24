8887b47afd24241ffddef975aa86f3138e4e44e9
Austin Kaburia <88529649+kaburia@users.noreply.github.com>
2023-02-24 12:23:26 +0300

    Add a cron tab and automate the boot up process

--- install.sh
+++ install.sh
@@ -2,44 +2,80 @@
 
 # Update and upgrade the system
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
-# # Install Python 3.8 and set it to path
-# sudo apt-get install python3.8
-# sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 
-# Install raspi-config
-sudo apt-get install -y raspi-config
+os=$(uname -a)
 
-# Enable camera and GPIO pins using raspi-config
-sudo raspi-config nonint do_camera 0
-sudo raspi-config nonint do_spi 0
-sudo raspi-config nonint do_i2c 0
-sudo raspi-config nonint do_serial 0
+sudo pip install virtualenv 
 
-# Install modules for controlling ultrasonic sensor, servo motor, and DC motor
-sudo apt-get install python3-gpiozero
-sudo apt-get install python3-pigpio
-sudo apt-get install python3-picamera
-sudo apt-get install python3-opencv
-sudo apt-get install python3-numpy
-sudo apt-get install python3-scipy
-sudo apt-get install python3-pandas
-sudo apt-get install python3-matplotlib
-sudo apt-get install python3-tk
-sudo apt-get install python3-serial
+if [[ $os == *"Kali"* ]]; then
+  # Install Python 3.8 and set it to path
+  echo "Kali Linux detected."
+  sudo apt install python3.8 python3-pip
+  sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+  
+  python3.8 -m virtualenv CMS
+#   virtualenv -p /usr/bin/python3.8 CMS
+  
+  # Install raspi-config
+  sudo apt-get install -y raspi-config
+  
+  
+  # Enable camera and GPIO pins using raspi-config
+  sudo raspi-config nonint do_camera 0
+  sudo raspi-config nonint do_spi 0
+  sudo raspi-config nonint do_i2c 0
+  sudo raspi-config nonint do_serial 0
+  
+elif [[ $os == *"raspbian"* ]]; then
+  echo "Raspberry Pi OS detected."
+  python3 -m virtualenv CMS
+else
+  echo "Unknown operating system."
+fi
 
-# Set up the Coral USB accelerator
-cd ~
-wget https://dl.google.com/coral/edgetpu_api/edgetpu_api_latest.tar.gz -O edgetpu_api_latest.tar.gz
-tar xzf edgetpu_api_latest.tar.gz
-cd edgetpu_api
-bash ./install.sh
+# Activate the virtual environment
+source CMS/bin/activate
 
-# # Compile the YOLOv5s model to work with the Coral accelerator
-# cd ~
-# git clone https://github.com/ultralytics/yolov5.git
-# cd yolov5
-# sudo apt-get install protobuf-compiler libprotoc-dev
-# sudo pip install -r requirements.txt
-# python models/export.py --weights yolov5s.pt --img 640 --batch 1 --include package='edgetpu' --include file=tflite_convert
+# Install dependencies
+pip install -r requirements.txt
+
+# Set up the coral USB accelerator
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install -y libedgetpu1-std
+sudo apt-get install python-pycoral
+
+# Check the exit status of the command
+if [ $? -eq 0 ]; then
+    echo "Command ran successfully"
+else
+    echo "Command failed"
+    python -m pip install --extra-index-url https://google-coral.github.io/py-repo/ pycoral~=2.0
+fi
+
+# Run the script
+cd controls/
+
+# Get the device's IP address
+ip=$(hostname -I | awk '{print $1}')
+
+# Replace the host parameter in the Python script with the IP address
+sed -i "s/app.run(host=.*/app.run(host='${IP_ADDR}', debug=False, port=5500)/" controls.py
+
+# Set a cronjob to start on boot with the ip address as host
+# Write out current crontab to a file
+crontab -l > mycron
+
+# Echo new cron into cron file
+echo "@reboot /usr/bin/python3 /controls.py" >> mycron
+
+# Install new cron file
+crontab mycron
+
+# Remove the temporary file
+rm mycron
